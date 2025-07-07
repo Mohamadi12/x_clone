a6097d773ba7ab9f9ace108e4b569c498d138b40
Nana Mohamadi <adilnana78@gmail.com>
2025-07-07 23:16:34 +0000

    x_clone

---
+++ backend/src/server.js
@@ -0,0 +1,16 @@
+import express from "express"
+import { ENV } from "./config/env.js"
+import { connectDB } from "./config/db.js"
+
+const app = express()
+
+
+connectDB()
+
+app.get("/", (req, res) => {
+    res.send("Server is running")
+})
+
+app.listen(ENV.PORT, () => {
+    console.log(`Server is running on port: http://localhost:${ENV.PORT}`)
+})
